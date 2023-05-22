38eb555d5e07068ad0bc988c4a4cb2dd7e908b2d
yrajeshtechsierra <rajesh.yadav@techsierra.in>
2023-05-22 16:22:47 +0530

    added cors

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const app = express();
 dotenv.config();
 
-const db = process.env.MONGO_URI
-console.log(db)
+const db = process.env.MONGO_URI;
 
 mongoose.connect(db, { useNewUrlParser: true })
     .then(() => {
         console.log("Mongodb connected")
     }).catch((error) => console.log(error));
+
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("server is running")
